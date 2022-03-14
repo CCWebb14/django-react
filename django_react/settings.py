53534e679991a05d485ff44d54026e2cb64f8f29
Cooper Webb <CCWebb14@gmail.com>
2022-03-14 15:54:30 -0700

    converted sqlite settings to postgreSQL

--- django_react/settings.py
+++ django_react/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'django_react.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chat_database',
+        'USER': 'admin',
+        'PASSWORD': 'strongpassword123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
