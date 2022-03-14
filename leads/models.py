d2934d56b9f9f576f5fb9125a453d68abdf7b082
Cooper Webb <CCWebb14@gmail.com>
2022-03-13 23:51:57 -0700

    first commit initialization

---
+++ leads/models.py
@@ -0,0 +1,10 @@
+import email
+from django.db import models
+
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
