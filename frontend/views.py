e0cf2ea2e51604786c8a63d22fbbf4e56e81bab3
CCWebb14 <CCWebb14@gmail.com>
2022-03-31 14:38:12 -0700

    requirements.txt generated

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def index(request):
+    print(request.body)
     return render(request, 'frontend/index.html')
+
 
 
